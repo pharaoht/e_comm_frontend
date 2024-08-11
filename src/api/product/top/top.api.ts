0989f87d9c9ecf495d053892a9369ae39c0903fd
pharaoh <pharaohmanson@gmail.com>
2024-08-10 20:20:00 -0400

    navbar refactor

---
+++ src/api/product/top/top.api.ts
@@ -0,0 +1,5 @@
+
+
+
+//Instead of using getStaticProps, you can use getServerSideProps to fetch the data on every request. 
+//This ensures that the user always gets the latest data.
---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,44 @@
+import { GetServerSideProps  } from 'next';
+
+
+// export const getTopById: GetServerSideProps  = async ( context ) => {
+
+//     const { id } = context.query;
+
+//     const url = `http:localhost:8000/api/tops/${id}`;
+
+//     const response = await fetch(url);
+
+//     if(!response.ok){
+//         return {
+//             notFound: true
+//         }
+//     }
+
+//     const top = await response.json();
+
+//     return {
+//         props: {
+//             top,
+//         }
+//     };
+
+//     //Instead of using getStaticProps, you can use getServerSideProps to fetch the data on every request. 
+//     //This ensures that the user always gets the latest data.
+// };
+
+
+
+
+
+const ProductPage = ({ tops }: {tops: any}) => {
+
+
+    return (
+        <div>
+            hi
+        </div>
+    )
+};
+
+export default ProductPage;
